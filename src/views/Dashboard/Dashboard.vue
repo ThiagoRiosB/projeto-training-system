16ecbca08dab16f1307ed535f5861ca94744e724
ThiagoRiosB <123420372+ThiagoRiosB@users.noreply.github.com>
2023-09-14 16:45:08 -0300

    refactor: Ajustei o alinhamento da página "Dashboard".

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -2,9 +2,12 @@
   <v-app theme="dark">
     <v-navigation-drawer color="gray-darken-1" v-model="isDrawerOpen">
       <v-list>
-        <v-list-item prepend-icon="mdi-home"><router-link class="text-decoration-none text-white" to="/dashboard">Home</router-link></v-list-item>
-        <v-list-item prepend-icon="mdi-account"><router-link class="text-decoration-none text-white" to="/gerenciar-alunos">Gerenciar alunos</router-link></v-list-item>
-        <v-list-item prepend-icon="mdi-weight-lifter"><router-link class="text-decoration-none text-white" to="/gerenciar-exercicios">Gerenciar exercícios</router-link></v-list-item>
+        <v-list-item prepend-icon="mdi-home"><router-link class="text-decoration-none text-white"
+            to="/dashboard">Home</router-link></v-list-item>
+        <v-list-item prepend-icon="mdi-account"><router-link class="text-decoration-none text-white"
+            to="/gerenciar-alunos">Gerenciar alunos</router-link></v-list-item>
+        <v-list-item prepend-icon="mdi-weight-lifter"><router-link class="text-decoration-none text-white"
+            to="/gerenciar-exercicios">Gerenciar exercícios</router-link></v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar flat class="border-b" color="gray-darken-1">
@@ -15,28 +18,29 @@
         </div>
         <v-btn icon>
           <v-menu>
-            <template #activator="{props}">
+            <template #activator="{ props }">
               <v-avatar v-bind="props">
                 <v-icon color="white" icon="mdi-account-circle" size="50"></v-icon>
               </v-avatar>
             </template>
             <v-card min-width="200px">
               <v-card-text>
-              <div class="mx-auto text-center">
-                <v-avatar >
-                  <v-icon color="white" icon="mdi-account-circle" size="50"></v-icon>               
-                </v-avatar> 
-                <v-card-title >{{ userInfo.name }}</v-card-title>
-                <v-divider class="my-3"></v-divider>
-                <v-btn rounded variant="text"><router-link class="text-decoration-none text-white" to="/">Sair</router-link></v-btn>
-              </div>        
-            </v-card-text>
+                <div class="mx-auto text-center">
+                  <v-avatar>
+                    <v-icon color="white" icon="mdi-account-circle" size="50"></v-icon>
+                  </v-avatar>
+                  <v-card-title>{{ userInfo.name }}</v-card-title>
+                  <v-divider class="my-3"></v-divider>
+                  <v-btn rounded variant="text"><router-link class="text-decoration-none text-white"
+                      to="/">Sair</router-link></v-btn>
+                </div>
+              </v-card-text>
             </v-card>
           </v-menu>
         </v-btn>
       </template>
     </v-app-bar>
-    
+
     <v-main class="ma-15">
       <v-container>
         <h1 class="text-h4">Bem vindo, {{ userInfo.name }}</h1>
@@ -47,21 +51,23 @@
               <v-text class="pa-2 text-white text-center">{{ dashboardInfo.amount_students }} Alunos cadastrados</v-text>
               <v-btn color="orange" class="bg-orange-accent-3" variant="outlined">
                 <router-link class="text-decoration-none text-white" to="/gerenciar-alunos/">
-              Adicionar</router-link>
-             </v-btn>
-          </v-card>
+                  Adicionar</router-link>
+              </v-btn>
+            </v-card>
 
           </v-col>
           <v-col cols="12" md="4" lg="3">
             <v-card color="orange" style="width: 300px;" variant="outlined" class="d-flex flex-column">
-                <img style="width: 300px;"  src="../../img/img-card-exercicios.png" alt="card-exercicios">
-                <v-text class="pa-2 text-white text-center">{{ dashboardInfo.amount_exercises }} Exercícios cadastrados</v-text>
-                    <v-btn color="orange" class="bg-orange-accent-3" variant="outlined"><router-link class="text-decoration-none text-white" to="/gerenciar-exercicios">
-                    Adicionar</router-link>
-                   </v-btn>
-          </v-card>
+              <img style="width: 300px;" src="../../img/img-card-exercicios.png" alt="card-exercicios">
+              <v-text class="pa-2 text-white text-center">{{ dashboardInfo.amount_exercises }} Exercícios
+                cadastrados</v-text>
+              <v-btn color="orange" class="bg-orange-accent-3" variant="outlined"><router-link
+                  class="text-decoration-none text-white" to="/gerenciar-exercicios">
+                  Adicionar</router-link>
+              </v-btn>
+            </v-card>
           </v-col>
-        </v-row>   
+        </v-row>
       </v-container>
     </v-main>
     <v-footer class="bg-black">
@@ -72,23 +78,23 @@
 
 <script>
 import axios from 'axios'
- import {ref} from 'vue'
-  const isDrawerOpen = ref(false)
+import { ref } from 'vue'
+const isDrawerOpen = ref(false)
 
-  export default {
+export default {
   data() {
     return {
       dashboardInfo: {},
       userInfo: {},
-      isDrawerOpen : false
+      isDrawerOpen: false
     }
   },
-    mounted(){
-      axios.get('http://localhost:3000/dashboard')
+  mounted() {
+    axios.get('http://localhost:3000/dashboard')
       .then(res => this.dashboardInfo = res.data)
       .catch(error => console.log(error))
 
     this.userInfo = JSON.parse(localStorage.getItem('user-info')) || null
-    }
   }
+}
 </script>
