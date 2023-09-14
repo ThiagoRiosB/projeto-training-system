a678db1b57f0662ab78b9a7282f06384f24afb5c
ThiagoRiosB <123420372+ThiagoRiosB@users.noreply.github.com>
2023-09-14 17:23:48 -0300

    Refactor: Alinhei o código da página "Gerenciar Alunos".

--- src/views/GerenciarExercicios/GerenciarExercicios.vue
+++ src/views/GerenciarExercicios/GerenciarExercicios.vue
@@ -49,7 +49,7 @@
         <v-divider class="my-3 "></v-divider>
         <v-form ref="form" class="d-flex" @submit.prevent="handleSubmit">
           <v-text-field class="mr-15" v-model="description"
-          :rules="[value => validateName(value) || 'Exercício inválido!']" variant="outlined" type="text"
+            :rules="[value => validateName(value) || 'Exercício inválido!']" variant="outlined" type="text"
             label="Digite o nome do exercício" placeholder="Digite o nome do exercício" />
           <v-btn class="pl-2 text-white" color="orange" variant="outlined" type="submit">Cadastrar</v-btn>
         </v-form>
